a29d8660ecf8744ac98fa597c8947d80bc9a455e
Luca <lucadeste21@gmail.com>
2022-04-11 16:43:56 +0200

    add addaddress function

--- txbot.py
+++ txbot.py
@@ -45,17 +45,40 @@ def starting(message):
     else:
         print('qualcosa non va')
 
+def addAddr(message):
+    chat_id = message.chat.id
+    try:
+        print('addAddr Started')
+        bot.delete_message(chat_id,message.id)
+        if message.text == 'Back':
+            print('User want to go back')
+        elif message.text[0:5] == 'terra':
+            print('User want to monitor terra address')
+        else:
+            print('i dunno wat user want to do')
+    except Exception as e:
+        print(e)
+
 def handleResponse(message):
     chat_id=message.chat.id
     try:
         print('handleResponse function called')
         if message.text == 'Add Address':
             print('called add address from handle response')
-            addAddr(message)
+            bot.delete_message(message.chat.id,message.id)
+            markup = types.ReplyKeyboardMarkup()
+            itemBack = types.KeyboardButton('Back')
+            markup.row(itemBack)
+            msg = bot.send_message(chat_id, "Please, send me the address that you want to monitor. Please remember that for now you can only monitor one address at time.", reply_markup=markup)
+            bot.register_next_step_handler(msg,addAddr)
         elif message.text == "Remove Address":
             print('called Remove Address from handle response')
+            removeAddr(message)
         elif message.text == 'info':
             print('called info from handle response')
+            info(message)
         else:
             print('i dunno wat that is')
+    except Exception as e:
+        print(e)
 bot.polling()
